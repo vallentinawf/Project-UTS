d111f4a403a4da0c5c8c12a0e9e86789eb9ee047
Vallentina Wahyu F <79146280+vallentinawf@users.noreply.github.com>
2021-04-10 00:04:14 +0800

    Add files via upload

---
+++ CustomerData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kopilih
+{
+    public class CustomerData
+    {
+        protected string name;
+        protected double phoneNumber;
+        protected string address;
+
+
+        public virtual void Insert()
+        {
+            CustomerData customer = new CustomerData();
+
+            Console.WriteLine("What is Your Name?  ");
+            customer.name = Console.ReadLine();
+
+            Console.WriteLine("What is Your Phone Number? ");
+            customer.phoneNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("And Your Address? ");
+            customer.address = Console.ReadLine();
+
+
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kopilih
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            MainMenu kopi = new MainMenu();
+            kopi.Greet();
+            kopi.MainOrdering();
+            kopi.Insert();
+            kopi.Conlusing();
+
+        }
+
+    }
+}
